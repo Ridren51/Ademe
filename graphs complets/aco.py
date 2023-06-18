38badac70dbe9af7d1b05c5a156edab05ff39cf3
hugo <hugochampy@gmail.com>
2023-06-18 23:18:33 +0200

    refacto + docu + nettoyage

--- graphs complets/aco.py
+++ graphs complets/aco.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def aco(graph:Graph, start_node, num_ants:int = 10, alpha:int = 1, beta:int = 2, evaporation:float = 0.5, already_visited_penalty:float = 0.5, iterations:int = 10):
     """
+    heuristic aco algorithm for finding the shortest path in a graph
     :param graph: graph to use
     :param start_node: node to start from
     :param num_ants: number of ants to use
@@ -16,30 +17,22 @@ def aco(graph:Graph, start_node, num_ants:int = 10, alpha:int = 1, beta:int = 2,
     :return: tuple of (best path cost, best path)
     """
 
-
     best_path = []
-
     start_node = str(start_node)
-    # print("aco_proto", start_node)
-    # print("nodes", self.nodes)
-    # print("node", self.nodes[start_node])
-
     start_time = time.time()
 
-    max_iterations_without_improvement = len(graph.nodes)*2
+    max_iterations_without_improvement = len(graph.nodes)*2 #max number of iterations without improvement before stopping
     for _ in range(iterations): # Run ant colony optimization for a fixed number of iterations
         print("iteration", _)
         paths = []
         for _ in range(num_ants): # Create ant agents
             # print("ant", _)
-            current_city = graph.nodes[start_node]
-            unvisited_cities = list(graph.nodes.keys())
+            current_city = graph.nodes[start_node] #start from start node
+            unvisited_cities = list(graph.nodes.keys()) #list of unvisited cities
             path = []
             edges = []
             cost = 0
             last_city = None
-
-            iteration_time = time.time()
 
             iterations_without_improvement = 0
             last_size_unvisited_cities=len(unvisited_cities)
@@ -52,59 +45,67 @@ def aco(graph:Graph, start_node, num_ants:int = 10, alpha:int = 1, beta:int = 2,
                 #choose next city
                 for neighbor in current_city.neighbors:
 
-                    edge = graph.get_edge(current_city.node_name, neighbor)
+                    edge = graph.get_edge(current_city.node_name, neighbor) #get edge between current city and neighbor
                     pheromone = edge.pheromone ** alpha  # Calculate pheromone value
                     distance = 1/edge.weight ** beta if edge.weight != 0 else 0
-                    score = edge.pheromone * distance if edge.pheromone != 0 else 1*distance
+                    score = pheromone * distance if pheromone != 0 else 1*distance
 
                     if not unvisited_cities and neighbor == start_node:
+                        #if all cities have been visited, give a big bonus to the path that ends in the start node
                         score = score * 1000
                     elif neighbor in path:
                         if neighbor == last_city.node_name:
+                            # penalize going back to the last city
                             score = score/1000
                         score = already_visited_penalty * score # penalize already visited cities to avoid loops
+
                     total += score
                     neighbor_choice_probabilities.append(score)
 
                 probabilities = []
                 for p in neighbor_choice_probabilities:
                     if p == 0:
-                        probabilities.append(0)
+                        probabilities.append(0) # avoid division by 0
                         continue
                     probabilities.append(p / total)  # Calculate probabilities
                 if current_city.node_name in unvisited_cities:
-                    unvisited_cities.remove(current_city.node_name)
+                    unvisited_cities.remove(current_city.node_name) #remove current city from unvisited cities
                 path.append(current_city.node_name)
 
 
                 if len(unvisited_cities) < last_size_unvisited_cities:
+                    #if we visited a new city, reset the number of iterations without improvement
                     iterations_without_improvement = 0
                 else:
+                    #if we didn't visit a new city, increment the number of iterations without improvement
                     iterations_without_improvement += 1
                 last_size_unvisited_cities = len(unvisited_cities)
                 if iterations_without_improvement > max_iterations_without_improvement:
-                    # print("stuck")
+                    #if we didn't visit a new city for too long, stop
                     break
-                # print("current_city", unvisited_cities)
 
                 last_city = current_city
                 current_city = graph.nodes[np.random.choice(current_city.neighbors, p=probabilities)]  # Choose next city
-                edges.append(graph.get_edge(last_city.node_name, current_city.node_name))
-                cost += graph.get_edge(last_city.node_name, current_city.node_name).weight
+                edges.append(graph.get_edge(last_city.node_name, current_city.node_name)) #add edge to path
+                cost += graph.get_edge(last_city.node_name, current_city.node_name).weight #add edge weight to cost
 
             if iterations_without_improvement < max_iterations_without_improvement:
+                #avoid adding paths when exceeding the number of iterations without improvement
+
+                #add the return to start node
                 path.append(start_node)
                 cost += graph.get_edge(last_city.node_name, start_node).weight
                 paths.append((cost,path))
 
-                for edge in list(set(edges)):
+                for edge in list(set(edges)): # Update pheromone values on edges
                     edge.pheromone += 1 / cost
                     edge.pheromone *= (1 - evaporation)  # Evaporate pheromone on all edges
         if not paths:
             best_path = ('no reasonable path found', [])
             continue
-        best_path = min(paths, key=lambda x: x[0])
+        best_path = min(paths, key=lambda x: x[0]) # set the new best path
     print("time", (time.time()-start_time)*1000,"ms")
     print("best cost", best_path)
     print('length', len(best_path[1]))
-    return best_path
+
+    return best_path # Return best path
--- graphs complets/utils.py
+++ graphs complets/utils.py
@@ -7,23 +7,32 @@ from aco import aco
 
 class Utils:
     def instance_starter(self, instance:object=None, instance_size=-1):
+        """
+        creates a graph instance if none is provided
+        :param instance: instance of Graph class
+        :param instance_size: number of nodes in the graph to generate
+        :return: instance of Graph class
+        """
 
-        if instance is None:
+        if instance is None: #if no instance is provided, generate a random one
             grapher = Graph()
             if instance_size != -1:
                 grapher.generate_random_graph(instance_size)
             else:
                 grapher.generate_random_graph(rd.randint(10, 100))
 
-        else:
+        else: #if an instance is provided, use it
             grapher = instance
         return grapher
-    def performance_test_multiple_instances(self, func, func_params:dict, iterations:int=1, instance_size:int=-1): #wrapper for performance test
+
+    def performance_test_multiple_instances(self, func, func_params:dict, iterations:int=1, instance_size:int=-1):
         """
+        wrapper for performance test on multiple instances of the same size outputting to a csv file
         :param func: function to test
         :param func_params: default parameters of the function
         :param iterations: number of iterations to run the test
         :param instance_size: number of nodes in the graph
+        :return None
         """
 
         import csv
@@ -31,39 +40,40 @@ class Utils:
         print("Running performance test for ", func.__name__, " with ", iterations, " iterations and ", instance_size, " nodes")
 
         filename=f'vendor/benchmarks/{func.__name__}/'
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        os.makedirs(os.path.dirname(filename), exist_ok=True) #create folder if it doesn't exist
 
-        with open(f"{filename}/{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.csv", mode='w', newline='') as benchfile:
-            writer = csv.writer(benchfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(["iteration", "runtime (ms)", "CPU time (ms)", "memory (mb)", "nb_nodes", "nb_edges", "cost", "path"])
+        with open(f"{filename}/{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.csv", mode='w', newline='') as benchfile: #open file
+            writer = csv.writer(benchfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) #create csv writer
+            writer.writerow(["iteration", "runtime (ms)", "CPU time (ms)", "memory (mb)", "nb_nodes", "nb_edges", "cost", "path"]) #write header
 
             for iteration in range(iterations):
-                grapher=self.instance_starter(instance_size=instance_size)
+                grapher=self.instance_starter(instance_size=instance_size) #create graph instance
 
                 start_time = time.time()
                 start_cpu_time = psutil.Process().cpu_times().user  # Measure CPU time before running the algorithm
                 start_memory_usage = psutil.Process().memory_info().rss / 1024 / 1024  # Convert to megabytes
 
 
-                result = func(graph=grapher,**func_params)
-                print("result: ", result)
+                result = func(graph=grapher,**func_params) #run the algorithm
 
 
                 end_cpu_time = psutil.Process().cpu_times().user  # Measure CPU time after running the algorithm
                 end_memory_usage = psutil.Process().memory_info().rss / 1024 / 1024  # Convert to megabytes
                 end_time = time.time()
 
+                #write results to csv
                 writer.writerow([iteration, (end_time - start_time)*1000, (end_cpu_time - start_cpu_time)*1000, end_memory_usage - start_memory_usage, len(grapher.nodes), len(grapher.edges), result[0],result[1]])
         benchfile.close()
 
-    def performance_test(self, func, func_params:dict, iterations:int=1, instance_size:int=-1, instance: object = None): #wrapper for performance test
-
+    def performance_test(self, func, func_params:dict, iterations:int=1, instance_size:int=-1, instance: object = None):
         """
+        wrapper for performance test on a same instance outputting to a csv file
         :param func: function to test
         :param func_params: default parameters of the function
         :param iterations: number of iterations to run the test
         :param instance_size: number of nodes in the graph
         :param instance: adjacency matrix of the graph
+        :return None
         """
 
         import os
@@ -77,36 +87,36 @@ class Utils:
 
 
         filename = f'vendor/benchmarks/{func.__name__}/'
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        os.makedirs(os.path.dirname(filename), exist_ok=True) # create folder if it doesn't exist
 
-        with open(f"{filename}/{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.csv", mode='w', newline='') as benchfile:
-            writer = csv.writer(benchfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(["iteration", "runtime (ms)", "CPU time (ms)", "memory (mb)", "nb_nodes", "nb_edges", "cost", "path"])
+        with open(f"{filename}/{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.csv", mode='w', newline='') as benchfile: # open file
+            writer = csv.writer(benchfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) # create csv writer
+            writer.writerow(["iteration", "runtime (ms)", "CPU time (ms)", "memory (mb)", "nb_nodes", "nb_edges", "cost", "path"]) # write header
 
             for iteration in range(iterations):
                 start_time = time.time()
                 start_cpu_time = psutil.Process().cpu_times().user  # Measure CPU time before running the algorithm
                 start_memory_usage = psutil.Process().memory_info().rss / 1024 / 1024  # Convert to megabytes
 
-                result = func(graph=grapher, **func_params)
-                print("result: ", result)
+                result = func(graph=grapher, **func_params) # run the algorithm
 
                 end_cpu_time = psutil.Process().cpu_times().user  # Measure CPU time after running the algorithm
                 end_memory_usage = psutil.Process().memory_info().rss / 1024 / 1024  # Convert to megabytes
                 end_time = time.time()
 
+                # write results to csv
                 writer.writerow([iteration, (end_time - start_time)*1000, (end_cpu_time - start_cpu_time)*1000, end_memory_usage - start_memory_usage, len(grapher.nodes), len(grapher.edges), result[0], result[1]])
         benchfile.close()
 
 
-    def aco_parameters_test(self, parameters:dict, iterations:int=1000, instance_size:int=-1, instance: object = None):    #wrapper which tests a function with varying parameters
+    def aco_parameters_test(self, parameters:dict, iterations:int=1000, instance_size:int=-1, instance: object = None):
         """
-        :param func: function to test
-        :param func_params: default parameters of the function
+        method to test the parameters of the aco algorithm and output the results to a csv file
         :param parameters: dictionary of parameters to test with min and max values ex: {"param1": [min, max], "param2": [min, max]}
         :param iterations: number of iterations to run the test
         :param instance_size: number of nodes in the graph
         :param instance: adjacency matrix of the graph
+        :return None
         """
         import os
         import csv
@@ -115,37 +125,32 @@ class Utils:
         print("Running parameters test for aco with ", iterations, " iterations and ",
               instance_size, " nodes")
 
-        #test all parameters combinations possible with the given parameters and their min and max values
-        # for i, j, k in product(range(parameters['num_ants'][0], parameters['num_ants'][1]), range(parameters['alpha'][0], parameters['alpha'][1]), range(parameters['beta'][0], parameters['beta'][1]), range(parameters['iterations'][0], parameters['iterations'][1])):
-        #     # for l in product(range(parameters['beta'][0], parameters['beta'][1]), range(parameters['evaporation'][0], parameters['evaporation'][1])):
-        #     #     for m, n in product(range(parameters['already_visited_penalty'][0], parameters['already_visited_penalty'][1]), range(parameters['iterations'][0], parameters['iterations'][1])):
-        #     #         self.performance_test(func, {'num_ants': i, 'alpha': j, 'beta': k, 'evaporation': l, 'already_visited_penalty': m, 'iterations': n}, iterations, instance_size, instance)
-        #     print(i, j, k)
-        grapher = self.instance_starter(instance=instance, instance_size=instance_size)
-        grapher.plot_graph()
+        grapher = self.instance_starter(instance=instance, instance_size=instance_size) #create graph instance
+
         filename = '../vendor/benchmarks/param_test/'
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        os.makedirs(os.path.dirname(filename), exist_ok=True) #create folder if it doesn't exist
 
 
 
-        with open(f"{filename}aco_{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.csv", mode='w', newline='') as benchfile:
-            writer = csv.writer(benchfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(["iteration", "runtime (ms)", "CPU time (ms)", "memory (mb)", "nb_nodes", "nb_edges", "parameters", "cost", "path"])
+        with open(f"{filename}aco_{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.csv", mode='w', newline='') as benchfile: #open file
+            writer = csv.writer(benchfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) #create csv writer
+            writer.writerow(["iteration", "runtime (ms)", "CPU time (ms)", "memory (mb)", "nb_nodes", "nb_edges", "parameters", "cost", "path"]) #write header
 
             for iteration in range(iterations):
                 start_time = time.time()
                 start_cpu_time = psutil.Process().cpu_times().user  # Measure CPU time before running the algorithm
                 start_memory_usage = psutil.Process().memory_info().rss / 1024 / 1024  # Convert to megabytes
 
+                #create random parameters in the range specified
                 func_params = {'num_ants': rd.randint(parameters['num_ants'][0],parameters['num_ants'][1]), 'alpha': rd.randint(parameters['alpha'][0],parameters['alpha'][1]), 'beta': rd.randint(parameters['beta'][0],parameters['beta'][1]), 'evaporation': rd.uniform(parameters['evaporation'][0],parameters['evaporation'][1]), 'already_visited_penalty': rd.uniform(parameters['already_visited_penalty'][0],parameters['already_visited_penalty'][1])}
-                print(func_params)
 
-                result = aco(graph=grapher, start_node=0, **func_params)
+                result = aco(graph=grapher, start_node=0, **func_params) #run the algorithm
 
                 end_cpu_time = psutil.Process().cpu_times().user  # Measure CPU time after running the algorithm
                 end_memory_usage = psutil.Process().memory_info().rss / 1024 / 1024  # Convert to megabytes
                 end_time = time.time()
 
+                #write results to csv
                 writer.writerow([iteration, (end_time - start_time) * 1000, (end_cpu_time - start_cpu_time) * 1000,
                                  end_memory_usage - start_memory_usage, len(grapher.nodes), len(grapher.edges), func_params, result[0],
                                  result[1]])
